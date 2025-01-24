9d1f1dfd63a5c95b7430dd1fe5be1560ee4eb64c
veda336 <69802960+veda336@users.noreply.github.com>
2025-01-24 19:08:05 +0530

    Update Main.java

--- TicTacToe-master/TicTacToe/src/Main.java
+++ TicTacToe-master/TicTacToe/src/Main.java
@@ -3,3 +3,26 @@ public class Main {
         TicTacToe ticTacToe = new TicTacToe();
     }
 }
+import React from 'react';
+
+const EmailToMailto = ({ sentence }) => {
+  const transformEmailToLink = (text) => {
+    const emailRegex = /\b[A-Za-z0-9._%+-]+@gmail\.com\b/g; // Regex to find Gmail addresses
+    return text.split(emailRegex).map((part, index, arr) => {
+      if (index < arr.length - 1) {
+        const emailMatch = text.match(emailRegex)[index];
+        return (
+          <React.Fragment key={index}>
+            {part}
+            <a href={`mailto:${emailMatch}`}>{emailMatch}</a>
+          </React.Fragment>
+        );
+      }
+      return part;
+    });
+  };
+
+  return <p>{transformEmailToLink(sentence)}</p>;
+};
+
+export default EmailToMailto;
